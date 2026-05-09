3119c555f7ff902baf4e9d8fdce2a5b360b17957
bestbetternow <162303100+bestbetternow@users.noreply.github.com>
2026-05-09 19:25:44 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 const path = require("path")
 module.exports = {
-    entry: [
-        "./public/build/babel/app.js",
-        "./public/build/babel/storage.js",
-        "./public/build/babel/categoryView.js",
-        "./public/build/babel/productView.js",
-    ],
+    entry: {
+        bundle: [
+            "./public/build/babel/app.js",
+            "./public/build/babel/storage.js",
+            "./public/build/babel/categoryView.js",
+            "./public/build/babel/productView.js",
+        ],
+        privacy: "./public/build/babel/privacyPage.js",
+    },
     output: {
-        path: path.resolve(__dirname, "build/webpack"),
-        filename: "bundle.js"
+        path: path.resolve(__dirname, "public/build/webpack"),
+        filename: "[name].js"
     },
     module: {
         rules: [{
